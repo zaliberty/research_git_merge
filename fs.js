02251e9571ac94667bff21bd91196025e06ae549
zaliberty <dream_vital@mail.ru>
2017-09-07 22:54:44 +0300

    add async readdir function

--- fs.js
+++ fs.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const filesInDir = fs.readdirSync(__dirname);
-console.log(filesInDir);
+console.log('list files from sync readdir', filesInDir);
+
+fs.readdir(__dirname, (error, files) => {
+    if(error) {
+        return console.error(error);
+    }
+
+    console.log('list files from async readdir: ', files)
+});
