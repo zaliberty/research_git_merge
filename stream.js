312d38f0b9268e0f12e0f1b2434802254e5ceebe
zaliberty <dream_vital@mail.ru>
2017-09-07 23:39:09 +0300

    using stream

--- stream.js
+++ stream.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const http = require('http');
 
 http.createServer((req, res) => {
-    fs.readFile('text.txt', (error, data) => {
-        if(error) {
-            throw error;
-        }
-        res.end(data);
-    });
+    // fs.readFile('text.txt', (error, data) => {
+    //     if(error) {
+    //         throw error;
+    //     }
+    //     res.end(data);
+    // });
+    const stream = fs.createReadStream('text.txt');
+    stream.pipe(res);
 }).listen(3000);
